22b0b17a75c2048bf66e63d19a94947f88a652a1
ArtiRajiv <aarthi.public@gmail.com>
2021-02-09 22:52:37 -0800

    Day2 Exercises

---
+++ CompletedExercises/Day2.java
@@ -0,0 +1,20 @@
+public class Day2 {
+	int i;
+	public static void main(String args[]) {
+
+		//ex 1
+		int i = 1;
+		do {
+			System.out.println(i);
+			i+= 2;
+		} while (i<=49);
+
+		// ex 2
+		System.out.println("\n");
+		char alphabet = 'a';
+		do {
+			System.out.println(alphabet);
+			alphabet++;
+		} while( alphabet <= 'z');
+	}
+}
